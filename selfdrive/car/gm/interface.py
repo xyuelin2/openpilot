1dad4964701c659ca55be10fefb75f53af6de337
Tim Wilson <twilsonco@gmail.com>
2022-05-18 19:27:20 -0600

    silverado lat lower ki,kp,kfRight

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -307,12 +307,12 @@ class CarInterface(CarInterfaceBase):
       ret.steerRateCost = .4
       ret.steerActuatorDelay = 0.11
       ret.lateralTuning.pid.kpBP = [0., 40.]
-      ret.lateralTuning.pid.kpV = [0.13, 0.23]
+      ret.lateralTuning.pid.kpV = [0.12, 0.2]
       ret.lateralTuning.pid.kiBP = [0., 40.]
-      ret.lateralTuning.pid.kiV = [0.02, 0.025]
+      ret.lateralTuning.pid.kiV = [0.01, 0.02]
       ret.lateralTuning.pid.kdBP = [0.]
       ret.lateralTuning.pid.kdV = [0.6]
-      ret.lateralTuning.pid.kf = 0.8 # when turning right. use with get_steer_feedforward_silverado()
+      ret.lateralTuning.pid.kf = 0.7 # when turning right. use with get_steer_feedforward_silverado()
       ret.lateralTuning.pid.kfLeft = 1. #  when turning left. use with get_steer_feedforward_silverado()
 
     elif candidate == CAR.SUBURBAN:

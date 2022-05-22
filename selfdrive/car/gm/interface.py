4940ad54e09c0e5dfedb65023b0a464c4a729468
Tim Wilson <twilsonco@gmail.com>
2022-05-22 15:10:21 -0600

    silverado apply most recent tune

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -307,13 +307,13 @@ class CarInterface(CarInterfaceBase):
       ret.steerRateCost = .4
       ret.steerActuatorDelay = 0.11
       ret.lateralTuning.pid.kpBP = [0., 40.]
-      ret.lateralTuning.pid.kpV = [0.12, 0.2]
+      ret.lateralTuning.pid.kpV = [0.12, 0.18]
       ret.lateralTuning.pid.kiBP = [0., 40.]
-      ret.lateralTuning.pid.kiV = [0.01, 0.02]
+      ret.lateralTuning.pid.kiV = [0.005, 0.015]
       ret.lateralTuning.pid.kdBP = [0.]
       ret.lateralTuning.pid.kdV = [0.6]
-      ret.lateralTuning.pid.kf = 0.7 # when turning right. use with get_steer_feedforward_silverado()
-      ret.lateralTuning.pid.kfLeft = 1. #  when turning left. use with get_steer_feedforward_silverado()
+      ret.lateralTuning.pid.kf = 0.8 # when turning right. use with get_steer_feedforward_silverado()
+      ret.lateralTuning.pid.kfLeft = 0.8 #  when turning left. use with get_steer_feedforward_silverado()
 
     elif candidate == CAR.SUBURBAN:
       ret.minEnableSpeed = -1. # engage speed is decided by pcmFalse

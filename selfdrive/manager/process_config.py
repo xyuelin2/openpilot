8be367d0d87b88724a3dd98c720e02079e5ed2db
Jason Shuler <jshuler@gmail.com>
2022-06-07 13:36:08 -0400

    enable bridge

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -51,7 +51,7 @@ procs = [
   PythonProcess("uploader", "selfdrive.loggerd.uploader", offroad=True),
   PythonProcess("statsd", "selfdrive.statsd", offroad=True),
 
-  NativeProcess("bridge", "cereal/messaging", ["./bridge"], onroad=False, callback=notcar),
+  NativeProcess("bridge", "cereal/messaging", ["./bridge"]), # JJS - always on for tuning
   PythonProcess("webjoystick", "tools.joystick.web", onroad=False, callback=notcar),
 
   # Experimental

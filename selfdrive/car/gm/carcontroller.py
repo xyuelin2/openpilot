7bd2c0856b5ee9e071a47bf68378ca7856a2c878
Eric Brown <eric@ebrown.net>
2023-05-26 20:48:00 -0600

    Remove extraneous CAN messages

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -83,29 +83,29 @@ class CarController:
         can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, CanBus.CHASSIS, self.apply_brake, idx, near_stop, at_full_stop))
 
     # Send dashboard UI commands (ACC status), 25hz
-    if (self.frame % 4) == 0:
-      send_fcw = hud_alert == VisualAlert.fcw
-      can_sends.append(gmcan.create_acc_dashboard_command(self.packer_pt, CanBus.POWERTRAIN, CC.enabled,
-                                                          hud_v_cruise * CV.MS_TO_KPH, hud_control.leadVisible, send_fcw))
+    # if (self.frame % 4) == 0:
+    #   send_fcw = hud_alert == VisualAlert.fcw
+    #   can_sends.append(gmcan.create_acc_dashboard_command(self.packer_pt, CanBus.POWERTRAIN, CC.enabled,
+    #                                                       hud_v_cruise * CV.MS_TO_KPH, hud_control.leadVisible, send_fcw))
 
     # Radar needs to know current speed and yaw rate (50hz),
     # and that ADAS is alive (10hz)
-    time_and_headlights_step = 10
-    tt = self.frame * DT_CTRL
+    # time_and_headlights_step = 10
+    # tt = self.frame * DT_CTRL
 
-    if self.frame % time_and_headlights_step == 0:
-      idx = (self.frame // time_and_headlights_step) % 4
-      can_sends.append(gmcan.create_adas_time_status(CanBus.OBSTACLE, int((tt - self.start_time) * 60), idx))
-      can_sends.append(gmcan.create_adas_headlights_status(self.packer_obj, CanBus.OBSTACLE))
+    # if self.frame % time_and_headlights_step == 0:
+    #   idx = (self.frame // time_and_headlights_step) % 4
+    #   can_sends.append(gmcan.create_adas_time_status(CanBus.OBSTACLE, int((tt - self.start_time) * 60), idx))
+    #   can_sends.append(gmcan.create_adas_headlights_status(self.packer_obj, CanBus.OBSTACLE))
 
-    speed_and_accelerometer_step = 2
-    if self.frame % speed_and_accelerometer_step == 0:
-      idx = (self.frame // speed_and_accelerometer_step) % 4
-      can_sends.append(gmcan.create_adas_steering_status(CanBus.OBSTACLE, idx))
-      can_sends.append(gmcan.create_adas_accelerometer_speed_status(CanBus.OBSTACLE, CS.out.vEgo, idx))
+    # speed_and_accelerometer_step = 2
+    # if self.frame % speed_and_accelerometer_step == 0:
+    #   idx = (self.frame // speed_and_accelerometer_step) % 4
+    #   can_sends.append(gmcan.create_adas_steering_status(CanBus.OBSTACLE, idx))
+    #   can_sends.append(gmcan.create_adas_accelerometer_speed_status(CanBus.OBSTACLE, CS.out.vEgo, idx))
 
-    if self.frame % self.params.ADAS_KEEPALIVE_STEP == 0:
-      can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
+    # if self.frame % self.params.ADAS_KEEPALIVE_STEP == 0:
+    #   can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
 
     # Show green icon when LKA torque is applied, and
     # alarming orange icon when approaching torque limit.

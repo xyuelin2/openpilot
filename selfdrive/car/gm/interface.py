83ebf7d3c378f158fa1621291ae761c8b4af0744
Eric Brown <eric@ebrown.net>
2023-06-08 09:12:47 -0600

    Disable cruise fault on CC only cars

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -5,7 +5,7 @@ from math import fabs
 from common.conversions import Conversions as CV
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.gm.values import CAR, CruiseButtons, \
-                                    AccState, CarControllerParams
+  AccState, CarControllerParams, CC_ONLY_CAR
 from selfdrive.car.interfaces import CarInterfaceBase
 
 ButtonType = car.CarState.ButtonEvent.Type
@@ -206,7 +206,7 @@ class CarInterface(CarInterfaceBase):
       events.add(EventName.belowEngageSpeed)
     if ret.cruiseState.standstill:
       events.add(EventName.resumeRequired)
-    if self.CS.pcm_acc_status == AccState.FAULTED:
+    if self.CS.pcm_acc_status == AccState.FAULTED and self.CP.carFingerprint not in CC_ONLY_CAR:
       events.add(EventName.accFaulted)
     if ret.vEgo < self.CP.minSteerSpeed:
       events.add(car.CarEvent.EventName.belowSteerSpeed)

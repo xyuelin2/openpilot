cff448e5038e583e6b41cc9380ff02bd58a90c51
Eric Brown <eric@ebrown.net>
2023-08-03 21:26:33 -0600

    Add release files

--- release/build_release.sh
+++ release/build_release.sh
@@ -8,6 +8,7 @@ cd $DIR
 
 BUILD_DIR=/data/openpilot
 SOURCE_DIR="$(git rev-parse --show-toplevel)"
+RELEASE_BRANCH="build-$(date -u +%Y%m%d)"
 
 if [ -f /TICI ]; then
   FILES_SRC="release/files_tici"
@@ -15,12 +16,6 @@ else
   echo "no release files set"
   exit 1
 fi
-
-if [ -z "$RELEASE_BRANCH" ]; then
-  echo "RELEASE_BRANCH is not set"
-  exit 1
-fi
-
 
 # set git identity
 source $DIR/identity.sh
@@ -30,7 +25,7 @@ rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 git init
-git remote add origin git@github.com:commaai/openpilot.git
+git remote add origin git@github.com:opgm/openpilot.git
 git checkout --orphan $RELEASE_BRANCH
 
 # do the files copy
@@ -45,7 +40,9 @@ cd $BUILD_DIR
 rm -f panda/board/obj/panda.bin.signed
 rm -f panda/board/obj/panda_h7.bin.signed
 
-VERSION=$(cat common/version.h | awk -F[\"-]  '{print $2}')
+# set version to today's date
+VERSION=$(date -u +%Y%m%d)
+#VERSION=$(cat common/version.h | awk -F[\"-]  '{print $2}')
 echo "#define COMMA_VERSION \"$VERSION-release\"" > common/version.h
 
 echo "[-] committing version $VERSION T=$SECONDS"
@@ -57,7 +54,7 @@ export PYTHONPATH="$BUILD_DIR"
 scons -j$(nproc)
 
 # release panda fw
-CERT=/data/pandaextra/certs/release RELEASE=1 scons -j$(nproc) panda/
+CERT=/data/pandaextra/certs/release scons -j$(nproc) panda/
 
 # Ensure no submodules in release
 if test "$(git submodule--helper list | wc -l)" -gt "0"; then
@@ -92,14 +89,13 @@ TEST_FILES="tools/"
 cd $SOURCE_DIR
 cp -pR -n --parents $TEST_FILES $BUILD_DIR/
 cd $BUILD_DIR
-RELEASE=1 selfdrive/test/test_onroad.py
+#RELEASE=1 selfdrive/test/test_onroad.py
 #selfdrive/manager/test/test_manager.py
 #selfdrive/car/tests/test_car_interfaces.py
-rm -rf $TEST_FILES
 
 if [ ! -z "$RELEASE_BRANCH" ]; then
   echo "[-] pushing release T=$SECONDS"
-  git push -f origin $RELEASE_BRANCH:$RELEASE_BRANCH
+  GIT_SSH_COMMAND="ssh -i /data/id_rsa_github" git push -f origin $RELEASE_BRANCH:$RELEASE_BRANCH
 fi
 
 echo "[-] done T=$SECONDS"
